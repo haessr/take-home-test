bfbc12fd894e07e61517122dfb53d51a449eee08
Haess Romani <hromaza@gmail.com>
2021-05-21 21:34:49 -0500

    Render commits information to the view
    - Adds commitsView.js, responsible to build the markup to be rendered

--- app/javascript/config.js
+++ app/javascript/config.js
@@ -1,4 +1,6 @@
-export const GITHUB_REPO_URL = "https://github.com/haessr/forkify";
-export const GITHUB_API_URL = "https://api.github.com/repos/haessr/forkify";
+export const REPO_OWNER = "haessr";
+export const REPO_NAME = "take-home-test";
+export const GITHUB_REPO_URL = `https://github.com/${REPO_OWNER}/${REPO_NAME}`;
+export const GITHUB_API_URL = `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}`;
 export const TIMEOUT_SECONDS = 10;
-export const RESULTS_PER_PAGE = 5;
+export const RESULTS_PER_PAGE = 10;
--- app/javascript/controllers/github_controller.js
+++ app/javascript/controllers/github_controller.js
@@ -2,14 +2,22 @@ import { Controller } from "stimulus";
 // import { GITHUB_REPO_URL } from "../config";
 import * as model from "../model";
 
+import commitsView from "../views/commitsView";
+
 export default class extends Controller {
   initialize() {
     console.log(`Accessing commits`);
-    this.load();
+    this.controlAPIResponse();
   }
 
-  async load() {
+  connect() {
+    console.log(`Connected`);
+  }
+
+  async controlAPIResponse() {
     await model.loadRepoCommits();
-    console.log(model.state.commits);
+    commitsView.parentElement = document.querySelector(".results");
+    commitsView.render(model.state.commits);
+    // console.log(model.state.commits);
   }
 }
