952642a7179c60a452f03495c3faf61148795528
Haess Romani <hromaza@gmail.com>
2021-05-21 23:01:34 -0500

    Add style to pagination section

--- app/javascript/views/paginationView.js
+++ app/javascript/views/paginationView.js
@@ -3,7 +3,7 @@ import View from "./View";
 class PaginationView extends View {
   addHandlerClick(handlerFunction) {
     this._parentElement.addEventListener("click", function (e) {
-      const btn = e.target.closest(".btn--inline");
+      const btn = e.target.closest(".pagination__btn");
       if (!btn) return;
 
       const goToPage = Number(btn.dataset.goto);
@@ -19,7 +19,7 @@ class PaginationView extends View {
     // Page 1, and there are other pages
     if (currentPage === 1 && numPages > 1) {
       return `
-          <button class="btn btn-primary btn--inline" data-goto="${
+          <button class="btn btn-primary pagination__btn pagination__btn--prev" data-goto="${
             currentPage + 1
           }">
             <span>Page ${currentPage + 1} &gt;</span>
@@ -29,7 +29,7 @@ class PaginationView extends View {
     // Last page
     if (currentPage === numPages && numPages > 1) {
       return `
-          <button class="btn btn-primary btn--inline" data-goto="${
+          <button class="btn btn-primary pagination__btn pagination__btn--prev" data-goto="${
             currentPage - 1
           }">
             <span>&lt; Page ${currentPage - 1}</span>
@@ -39,12 +39,12 @@ class PaginationView extends View {
     // Other page
     if (currentPage < numPages) {
       return `
-          <button class="btn btn-primary btn--inline" data-goto="${
+          <button class="btn btn-primary pagination__btn pagination__btn--prev" data-goto="${
             currentPage - 1
           }">
             <span>&lt; Page ${currentPage - 1}</span>
           </button>
-          <button class="btn btn-primary btn--inline" data-goto="${
+          <button class="btn btn-primary pagination__btn pagination__btn--prev" data-goto="${
             currentPage + 1
           }">
             <span>Page ${currentPage + 1} &gt;</span>
